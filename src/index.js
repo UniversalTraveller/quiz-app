afa0b1ebd24903dfc75c6cf4415ad86fdd19c3c2
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-06 02:09:54 +0200

    Add JS to buttons for answers and bookmarks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-console.clear();
-console.log("hallo welt!");
 let answersButton = document.querySelector('[js-data="show-answer-button"]');
-
+answersButton.addEventListener("click", onButtonClick);
 function onButtonClick() {
-  console.log("test");
   let answer = document.querySelector('[js-data="answer"]');
   answer.classList.toggle("question-card__answer--hidden");
+  if (answersButton.innerHTML.includes("Show Answer")) {
+    answersButton.innerHTML = "Hide Anser";
+  } else {
+    answersButton.innerHTML = "Show Answer";
+  }
 }
 
-answersButton.addEventListener("click", onButtonClick);
+let bookmarkButton = document.querySelector('[data-js="bookmark"]');
+bookmarkButton.addEventListener("click", onBookmarkClick);
+function onBookmarkClick() {
+  bookmarkButton.classList.toggle("question-card__bookmark-button--active");
+}
