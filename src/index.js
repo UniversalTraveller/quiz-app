127e778b3bdf9708dd52f305f442c02d5f3ed569
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-06 11:11:23 +0200

    Cosmetic changes in JS

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ answerButton.addEventListener("click", onButtonClick);
 function onButtonClick() {
   const answer = document.querySelector('[data-js="answer"]');
   answer.classList.toggle("question-card__answer--hidden");
-  if (answerButton.innerHTML.includes("Show Answer")) {
-    answerButton.innerHTML = "Hide Answer";
+  if (answerButton.innerText === "Show Answer") {
+    answerButton.innerText = "Hide Answer";
   } else {
-    answerButton.innerHTML = "Show Answer";
+    answerButton.innerText = "Show Answer";
   }
 }
 
