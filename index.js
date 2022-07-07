6d063ed8315cf398368a7d070005eb042691b11f
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-07 23:33:27 +0200

    New folder structure for components

---
+++ index.js
@@ -0,0 +1,5 @@
+import showAnswer from "./components/molecules/question-card/showAnswer.js";
+import setBookmark from "./components/molecules/question-card/setBookmark.js";
+
+showAnswer();
+setBookmark();
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import showAnswer from "./showAnswer.js";
-import setBookmark from "../question-card/setBookmark.js";
-
-showAnswer();
-setBookmark();
