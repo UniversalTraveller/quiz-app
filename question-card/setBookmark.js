4ebac13ec4e47a94aafe17d96d25b844c41cf3ec
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-07 20:58:24 +0200

    Add forEach to bookmark buttons

--- question-card/setBookmark.js
+++ question-card/setBookmark.js
@@ -1,13 +1,18 @@
 export default function setBookmark() {
-  const bookmarkButton = document.querySelector('[data-js="bookmark"]');
-  let isBookmarked = false;
-  bookmarkButton.addEventListener("click", () => {
-    if (isBookmarked) {
-      bookmarkButton.classList.remove("question-card__bookmark-button--active");
-      isBookmarked = false;
-    } else {
-      bookmarkButton.classList.add("question-card__bookmark-button--active");
-      isBookmarked = true;
-    }
+  const bookmarkButtons = document.querySelectorAll('[data-js="bookmark"]');
+  console.log(bookmarkButtons);
+  bookmarkButtons.forEach((bookmarkButton) => {
+    let isBookmarked = false;
+    bookmarkButton.addEventListener("click", () => {
+      if (isBookmarked) {
+        bookmarkButton.classList.remove(
+          "question-card__bookmark-button--active"
+        );
+        isBookmarked = false;
+      } else {
+        bookmarkButton.classList.add("question-card__bookmark-button--active");
+        isBookmarked = true;
+      }
+    });
   });
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import setBookmark from "../question-card/setBookmark.js";
 
 showAnswer();
 setBookmark();
-
-// auf inline/arrow funktion umbauen, boolean für isBookmarked und eventuell ternary operator, dateien anhand von komponenten ordnen
